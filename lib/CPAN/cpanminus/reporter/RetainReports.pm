ff799f5f7d17afffd515374ae291360a62243251
James E Keenan <jkeenan@cpan.org>
2017-12-15 14:37:52 -0500

    CPAN version 0.06

--- lib/CPAN/cpanminus/reporter/RetainReports.pm
+++ lib/CPAN/cpanminus/reporter/RetainReports.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use 5.10.1;
 use parent ('App::cpanminus::reporter');
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 use Carp;
 use File::Path qw( make_path );
 use File::Spec;
