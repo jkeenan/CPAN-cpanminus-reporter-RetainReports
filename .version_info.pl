b08fec3193fa2b11aff9eb78d84c059b5c19c2e9
James E Keenan <jkeenan@cpan.org>
2017-12-20 21:13:19 -0500

    CPAN version 0.08

--- .version_info.pl
+++ .version_info.pl
@@ -1,6 +1,6 @@
 %trans = (
-    version => { old => '0.06', new => '0.07' },
-    date    => { old => 'December 15 2017', new => 'December 20 2017' },
+    version => { old => '0.07', new => '0.08' },
+    date    => { old => 'December 20 2017', new => 'December 20 2017' },
     copyright => { old => '2017', new => '2017' },
 );
 
--- lib/CPAN/cpanminus/reporter/RetainReports.pm
+++ lib/CPAN/cpanminus/reporter/RetainReports.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use 5.10.1;
 use parent ('App::cpanminus::reporter');
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 use Carp;
 use File::Path qw( make_path );
 use File::Spec;
